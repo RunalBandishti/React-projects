c568cf1bf10ef10fd91b5dde6f047c4b3f253c18
Runal <bandishtirunal@gmail.com>
2021-06-25 17:23:14 +0530

    first commit

---
+++ Beach Resort Site/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import FeturedRooms from "../components/FeaturedRooms";
+export default function Home() {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeturedRooms />
+    </>
+  );
+}
---
+++ Beach Resort Site/src/pages/Rooms.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import RoomContainer from "../components/RoomContainer";
+const Rooms = () => {
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomContainer />
+    </>
+  );
+};
+
+export default Rooms;
